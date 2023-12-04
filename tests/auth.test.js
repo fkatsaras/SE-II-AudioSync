534eb01d8ee4eba05fa5eeb05e4ff7d495c4f039
Fotis Katsaras <karditsafotis@gmail.com>
2023-12-04 22:34:07 +0200

    Fixed test timeout | auth logic pending- Failing tests for 201, 201, 209

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -7,12 +7,18 @@ const app = require('../index.js');
 
 const BASE_URL = 'http://localhost:8080';
 
+
 test.before(async (t) => {
+
+    console.log('Starting server');
 
     //Start server before running tests
     t.context.server = http.createServer(app);
     t.context.prefixUrl = await listen(t.context.server);
     t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json'});
+    t.timeout(5000); // 5 seconds timeout
+
+    console.log('Started server');
 
 });
 
@@ -20,66 +26,87 @@ test.after.always((t) => {
     
     //Close server
     t.context.server.close();
+
+
 });
 
-//Succesful login
-test('User succesfully logs in with valid credentials', async (t) => {
 
-    const response = await t.context.got.post(`${BASE_URL}/login`, {
-        json: { username: 'valid-username', password: 'valid-password' },
+//Succesful login
+test('User login - Successful login', async (t) => {
+    // Define the user credentials
+    const loginCredentials = {
+        username: 'validUsername',
+        password: 'validPassword',
+    };
+
+    // Send a POST request to the login endpoint
+    const response = await got.post(`${BASE_URL}/login`, {
+        json: loginCredentials,
         responseType: 'json',
     });
 
-    t.is(response.statusCode, 200);
-    t.is(response.body.message, 'User succesfully logged in');
-
+    // Assert the response status code 
+    //Response doesnt have a body though
+    t.is(response.statusCode, 200, 'Expected status code 200');
+    
 });
+
 
 //Unsuccesful login
 test('User receives 401 for incorrect credentials during login', async (t) => {
 
-    const response = await t.context.got.post(`${BASE_URL}/login`, {
+    const loginCredentials = {
+        username: 'invalid-username',
+        password: 'invalid-password',
+    };
 
-        json: { username: 'invalid-username', password: 'invalid-password' },
+    //Send a POST response th the /login endpoint
+    const response = await got.post(`${BASE_URL}/login`, {
+
+        json: loginCredentials,
         responseType: 'json',
-        throwHttpErrors: false, 
     });
 
+    // Assert response status code
     t.is(response.statusCode, 401);
-    t.is(response.body.message, 'Incorrect username or password')
-})
+
+});
 
 //POST request to /register: Successful registration
-
 test('User successful;y registers with valid information', async (t) => {
 
-    const response = t.context.got.post(`${BASE_URL}/register`, {
-        json: { 
-            username: 'new-username',
-            email: 'new-email@example.com',
-            password: 'new-password'
-         },
+    const registerCredentials = {
+        username: 'new-username',
+        email: 'new-email@emaple.com',
+        password: 'new-password',
+    };
+
+    //Send a POST response to the /register endpoint
+    const response = await got.post(`${BASE_URL}/register`, {
+        json: registerCredentials,
         responseType: 'json',
     });
 
-    t.is(response.statusCode, 201);
-    t.is(response.body.message, 'User successfully registered');
-
+    // Assert response status code
+    t.is(response.statusCode, 201);    
 });
 
 // POST request to /register with duplicate registration data
 test('User receives 409 for duplicate registration', async (t) => {
-    
-    const response = await t.context.got.post(`${BASE_URL}/register`, {
-        json: {
-            username: 'existing-username',
-            email: 'existing-email@example.com',
-        },
+
+    const registerCredentials = {
+        username: 'existing-username',
+        email: 'existing-email@emaple.com',
+        password: 'existing-password',
+    };
+
+    //Send a POST request ti the /register endpoint
+    const response = await got.post(`${BASE_URL}/register`, {
+        json: registerCredentials,
         responseType: 'json',
-        throwHttpErrors: false, 
+        throwHttpErrors: false,
     });
 
     t.is(response.statusCode, 409);
-    t.is(response.body.message, 'User account already exists');
     
 });
