e482795ee06fb9a77b32a4ce614fb5bfc9836c94
Limpolits <iliasfits@outlook.com>
2023-12-20 00:40:10 +0200

    Added playlist test

--- tests/playlist.test.js
+++ tests/playlist.test.js
@@ -1,57 +1,134 @@
-const http = require('http');
-const test = require('ava');
-const listen = require('test-listen');
-const got = require('got');
+const http = require("http");
+const test = require("ava");
+const listen = require("test-listen");
+const got = require("got");
 
-const app = require('../index.js');
+const app = require("../index.js");
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
 
-// POST request to /playlists/new: Successful creation of a new playlist
-test('User can create a new playlist', async (t) => {
-    // Assume there is a valid user session token from a successful login
-    const sessionToken = 'validSessionToken';
-
-    // Playlist data to be created
-    const newPlaylistData = {
-        name: 'My Playlist',
-        description: 'A playlist created for testing purposes',
-        tracks: ['track1', 'track2', 'track3'],
-    };
-
-    // Send a POST request to the /playlists/new endpoint with the new playlist data
-    const response = await t.context.got.post('/playlists/new', {
-        headers: {
-            Authorization: `Bearer ${sessionToken}`,
-        },
-        json: newPlaylistData,
-        responseType: 'json',
-    });
-
-    // Assert response status code is 201 and check if the playlist is created successfully
-    t.is(response.statusCode, 201);
-    t.truthy(response.body.playlistId);
-    t.is(response.body.name, newPlaylistData.name);
-    t.is(response.body.description, newPlaylistData.description);
-    t.deepEqual(response.body.tracks, newPlaylistData.tracks);
+test.before(async (t) => {
+  // Start server before running tests
+  t.context.server = http.createServer(app);
+  t.context.prefixUrl = await listen(t.context.server);
+  t.context.got = got.extend({
+    prefixUrl: t.context.prefixUrl,
+    responseType: "json",
+  });
 });
 
-// POST request to /playlists/new without a valid session token
-test('User receives 401 when trying to create a new playlist without a valid session', async (t) => {
-    // Playlist data to be created
-    const newPlaylistData = {
-        name: 'My Playlist',
-        description: 'A playlist created for testing purposes',
-        tracks: ['track1', 'track2', 'track3'],
+test.after.always((t) => {
+  // Close server
+  t.context.server.close();
+});
+
+test.serial("Playlist Endpoint Tests", (t) => {
+  t.timeout(30000);
+
+  test("POST /playlists/new - Create a new playlist", async (t) => {
+    // Set up test data
+    const playlistData = { 
+        title:'Lo-Fi', 
+        description: 'LoFi music for study'
     };
 
-    // Send a POST request to the /playlists/new endpoint without a valid session token
-    const response = await t.context.got.post('/playlists/new', {
-        json: newPlaylistData,
-        responseType: 'json',
-        throwHttpErrors: false,
+    // Make the POST request
+    const response = await t.context.got.post("playlists/new", {
+      json: playlistData,
     });
 
-    // Assert response status code is 401
-    t.is(response.statusCode, 401);
+    // Check the response status code
+    t.is(response.statusCode, 201);
+  });
+
+  test("GET /playlists/{playlistId}/songs - Get songs in a playlist", async (t) => {
+    // Set up test data
+    const playlistId = 123;
+
+    // Make the GET request
+    const response = await t.context.got.get(`playlists/${playlistId}/songs`);
+
+    // Check the response status code
+    t.is(response.statusCode, 200);
+  });
+
+  test("POST /playlists/{playlistId}/songs - Add a song to a playlist", async (t) => {
+    // Set up test data
+    const playlistId = 123;
+    const songData = {
+        title: 'Hush', 
+        duration: 240, 
+        genre: 'Rock'
+      // Include song data here
+      // For example, { title: 'Song Title', duration: 240, genre: 'Rock' }
+    };
+
+    // Make the POST request
+    const response = await t.context.got.post(`playlists/${playlistId}/songs`, {
+      json: songData,
+    });
+
+    // Check the response status code
+    t.is(response.statusCode, 201);
+  });
+
+  test("DELETE /playlists/{playlistId}/songs - Remove a song from a playlist", async (t) => {
+    // Set up test data
+    const playlistId = 123;
+    const songId = 456;
+
+    // Make the DELETE request
+    const response = await t.context.got.delete(
+      `playlists/${playlistId}/songs?songId=${songId}`
+    );
+
+    // Check the response status code
+    t.is(response.statusCode, 204);
+  });
+
+  test("GET /playlists/{playlistId} - Get details of a playlist", async (t) => {
+    // Set up test data
+    const playlistId = 123;
+
+    // Make the GET request
+    const response = await t.context.got.get(`playlists/${playlistId}`);
+
+    // Check the response status code
+    t.is(response.statusCode, 200);
+  });
+
+  test("PUT /playlists/{playlistId} - Update playlist details", async (t) => {
+    // Set up test data
+    const playlistId = 123;
+    const playlistUpdateData = {
+        title: 'Updated Playlist Title', 
+        description: 'Updated playlist description'
+    };
+
+    // Make the PUT request
+    const response = await t.context.got.put(`playlists/${playlistId}`, {
+      json: playlistUpdateData,
+    });
+
+    // Check the response status code
+    t.is(response.statusCode, 200);
+  });
+
+  test("DELETE /playlists/{playlistId} - Delete a playlist", async (t) => {
+    // Set up test data
+    const playlistId = 123;
+
+    // Make the DELETE request
+    const response = await t.context.got.delete(`playlists/${playlistId}`);
+
+    // Check the response status code
+    t.is(response.statusCode, 204);
+  });
+
+  // Add more tests for other playlist endpoints
+});
+
+// Add cleanup steps if necessary
+test.after.always(async (t) => {
+  // Clean up resources or database records created during tests
 });
